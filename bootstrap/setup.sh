8cfbd7e92e8db6b2508ff7810fc9d50e1b38ce22
Barisi Kane <kanebarisi@gmail.com>
2017-08-24 08:07:51 +0100

    Cleanup and streamlining of setup script

--- bootstrap/setup.sh
+++ bootstrap/setup.sh
@@ -3,23 +3,19 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-sudo apt-get install -y php php7.0-fpm nginx
-sudo apt-get install -y letsencrypt 
+sudo apt-get install -y php php7.0-fpm nginx letsencrypt zip unzip php-xml
 
 cd ~
 sudo curl -sS https://getcomposer.org/installer | sudo php
 sudo mv composer.phar /usr/local/bin/composer
 sudo ln -s /usr/local/bin/composer /usr/bin/composer
+sudo php -i '--enable-xmlreader=shared' '--enable-xmlwriter=shared'
 
-sudo apt-get install -y zip unzip
-sudo apt-get install -y php-xml
 cd /usr/share/nginx/html
 sudo mkdir sites
 cd sites
-
-sudo apt-get install -y php-xml
-sudo php -i '--enable-xmlreader=shared' '--enable-xmlwriter=shared'
-
+sudo mkdir slayonmakeover.com
+sudo git clone https://github.com/barisi/barisi.git
 
 cd /etc/ssl/certs
 sudo openssl dhparam -out dhparam.pem 4096
